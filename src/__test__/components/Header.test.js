1a4c79d4c41821de6d44a18f430e8277042301fc
Hildegar Medina <medinahildegar1@gmail.com>
2022-01-04 18:54:53 -0300

    CI AND JEST

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/provideMock';
 import Header from '../../components/Header';
 
@@ -23,4 +24,15 @@ describe('<Header />', () => {
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
   });
 
+});
+
+describe('Header snapshot', () => {
+  test('Comprobar el snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
 });
